ff789ac129bb938bd5fcda9afbac51ed77d33fef
NguyenQuangHuy282002 <huynq2@bravebits.vn>
2023-11-11 15:30:02 +0700

    Update freelancer page

--- frontend-v2.0/src/Routes/ClientRoutes.tsx
+++ frontend-v2.0/src/Routes/ClientRoutes.tsx
@@ -1,25 +1,25 @@
 import Footer from 'Components/BeforeLoginComponents/Footer'
 import { SearchContextProvider } from 'Context/SearchContext'
-import EmailVerified from 'pages/EmailVerification/EmailVerified'
-import PleaseVerifiy from 'pages/EmailVerification/PleaseVerifiy'
 import AllContract from 'pages/ClientPages/AllContract'
 import AllJobPosts from 'pages/ClientPages/AllJobPost'
 import BringYourFreelancer from 'pages/ClientPages/BringYourTalent'
 import CreateContract from 'pages/ClientPages/CreateContract'
-import Freelancer from 'pages/ClientPages/Freelancer'
+import FreelancerList from 'pages/ClientPages/Freelancer'
 import FreelancerProfile from 'pages/ClientPages/FreelancerProfile'
+import JobDetailsBeforeProposals from 'pages/ClientPages/JobDetailsBeforeProposols'
 import Jobs from 'pages/ClientPages/Jobs'
 import PostJob from 'pages/ClientPages/PostJop'
 import Reports from 'pages/ClientPages/Reports'
 import ReviewProposals from 'pages/ClientPages/ReviewProposals'
+import EmailVerified from 'pages/EmailVerification/EmailVerified'
+import PleaseVerifiy from 'pages/EmailVerification/PleaseVerifiy'
 import Contract from 'pages/FreelancerPages/Contract'
 import TransactionHistory from 'pages/FreelancerPages/Reports/TransactionHistory'
 import Messages from 'pages/Messages'
 import Notifications from 'pages/Notifications'
 import PageNotFound from 'pages/PageNotFound'
-import React, { useState } from 'react'
-import { Routes, Route } from 'react-router-dom'
-import JobDetailsBeforeProposals from 'pages/ClientPages/JobDetailsBeforeProposols'
+import { useState } from 'react'
+import { Route, Routes } from 'react-router-dom'
 import Header from 'src/Components/ClientComponents/Header'
 
 export default function ClientRoutes() {
@@ -42,7 +42,7 @@ export default function ClientRoutes() {
           <Route path="/email-verification" element={<EmailVerified />} />
           <Route path="/sign-up/please-verify" element={<PleaseVerifiy />} />
           <Route path="/post-job" element={<PostJob />} />
-          <Route path="/freelancer" element={<Freelancer />} />
+          <Route path="/freelancer" element={<FreelancerList />} />
           <Route path="/freelancer-profile/:id" element={<FreelancerProfile />} />
           <Route path="/review-proposal/:id" element={<ReviewProposals />} />
           <Route path="/billing-history" element={<Reports />} />
