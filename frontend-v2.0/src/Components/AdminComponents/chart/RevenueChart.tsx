0c3b06551ad675b723ad975b6a612f20b368d24d
NguyenQuangHuy282002 <huynq2@bravebits.vn>
2023-12-11 16:56:33 +0700

    Fix bug

---
+++ frontend-v2.0/src/Components/AdminComponents/chart/RevenueChart.tsx
@@ -0,0 +1,56 @@
+import { Typography } from 'antd'
+import { ApexOptions } from 'apexcharts'
+import { format } from 'date-fns'
+import { useEffect, useState } from 'react'
+import ReactApexChart from 'react-apexcharts'
+import { getYearPaymentStats } from 'src/api/admin-apis'
+import { eRevenueChart } from './configs/eChart'
+
+function RevenueChart() {
+  const { Title, Paragraph } = Typography
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    getYearPaymentStats().then(res => setData(res.data))
+  }, [])
+
+  console.log(data)
+  return (
+    <>
+      <div id="chart">
+        <ReactApexChart
+          className="bar-chart"
+          options={{
+            ...(eRevenueChart.options as ApexOptions),
+            xaxis: { ...eRevenueChart.options.xaxis, categories: data.map(d => format(new Date(d._id), 'LLL')) },
+          }}
+          series={eRevenueChart.series.map(s => ({
+            ...s,
+            data: data.map(d => {
+              return d.totalAmount
+            }),
+          }))}
+          type="bar"
+          height={220}
+        />
+      </div>
+      <div className="chart-vistior">
+        <Title level={5}>Revenue</Title>
+        <Paragraph className="lastweek">{/* than last week <span className="bnb2">+30%</span> */}</Paragraph>
+        <Paragraph className="lastweek"></Paragraph>
+        {/* <Row>
+          {items.map((v, index) => (
+            <Col xs={6} xl={6} sm={6} md={6} key={index}>
+              <div className="chart-visitor-count">
+                <Title level={4}>{v.Title}</Title>
+                <span>{v.user}</span>
+              </div>
+            </Col>
+          ))}
+        </Row> */}
+      </div>
+    </>
+  )
+}
+
+export default RevenueChart
